d1af8860a03572f75d9a78da189598b5e575d935
SantiagoLovisotto <santiagolovisotto@gmail.com>
2025-01-09 00:25:04 -0300

    Repo creation

---
+++ src/main/java/com/lovidev/stock/system/Main.java
@@ -0,0 +1,13 @@
+package com.lovidev.stock.system;
+
+import gui.LoginGui;
+
+public class Main {
+
+		public static void main(String[] args) {
+				System.out.println("Hello World!");
+				
+				LoginGui loginGui = new LoginGui();
+				loginGui.setVisible(true);
+		}
+}
