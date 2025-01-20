350c23b70ec860c5a858842a946780e7c435315d
SantiagoLovisotto <santiagolovisotto@gmail.com>
2025-01-20 12:00:00 -0300

    Producto DAO

--- src/main/java/dao/ProductoDao.java
+++ src/main/java/dao/ProductoDao.java
@@ -1,4 +1,38 @@
 package dao;
 
+import modelo.Producto;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+
+import static conexion.DatabaseConnection.logger;
+
 public class ProductoDao {
+
+    public ProductoDao() {
+
+    }
+
+    public List<Producto> getAllProductos(Connection conn) {
+        String sql = "SELECT * FROM Producto";
+        List<Producto> productos = new ArrayList<>();
+        try (Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                productos.add(new Producto(
+                        rs.getInt("id"),
+                        rs.getString("titulo"),
+                        rs.getString("categoria"),
+                        rs.getInt("cantidad"),
+                        rs.getDouble("precio")
+                ));
+            }
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, "Error trying to show items in the database");
+        }
+        return productos;
+    }
+
+
 }
--- src/main/java/modelo/Producto.java
+++ src/main/java/modelo/Producto.java
@@ -1,4 +1,19 @@
 package modelo;
 
 public class Producto {
+
+    private int id;
+    private String titulo;
+    private String categoria;
+    private int cantidad;
+    private double precio;
+
+    public Producto(int id, String titulo, String categoria, int cantidad, double precio){
+        this.id = id;
+        this.titulo = titulo;
+        this.categoria = categoria;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
 }
